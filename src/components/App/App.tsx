a07951ffd68cc75cec972ae6d2a2047e4d7ece39
Olesia Erastova <olesia1205@yandex.ru>
2024-04-15 21:02:37 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import styles from './styles/styles.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <p>Hello test-assignment</p>
+    </div>
+  );
+}
+
+export default App;
