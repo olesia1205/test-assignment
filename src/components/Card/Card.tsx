0c886006ad08d72be4f8733cff6ca9d1377278c3
Olesia Erastova <olesia1205@yandex.ru>
2024-04-19 11:45:11 +0300

    style(components): add icons for Cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,20 +1,56 @@
-import cardImage from '../../images/heart_icon.svg';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { removeCard, toggleLike } from '../../store/cardsSlice';
+import { useAppDispatch } from '../../store/store';
 import styles from './styles/styles.module.css';
 
 type TPropsCard = {
   id: string;
-  image: string;
+  url: string;
+  liked: boolean;
 };
 
-function Card({ id, image }: TPropsCard) {
+function Card({ id, url, liked }: TPropsCard) {
+  const dispatch = useAppDispatch();
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleLikeClick = () => {
+    dispatch(toggleLike({ id }));
+  };
+
   return (
-    <article className={styles.card}>
-      <div className={styles.cardImg} style={{ backgroundImage: `url(${image})` }} />
-      <div className={styles.cardInfoblock}>
-        <img className={styles.cardLikeIcon} alt="like_icon" src={cardImage} />
-        <h2 className={styles.cardTitle}>{id}</h2>
-      </div>
-    </article>
+    <Link className={styles.link} to={`/images/${id}`}>
+      <article
+        className={styles.card}
+        onMouseOver={() => {
+          setIsHovered(true);
+        }}
+        onMouseLeave={() => {
+          setIsHovered(false);
+        }}
+        onFocus={() => {}}
+      >
+        <div className={styles.cardImg} style={{ backgroundImage: `url(${url})` }} />
+        <div className={styles.cardInfoblock}>
+          <button
+            className={
+              liked
+                ? `${styles.cardLikeButton} ${styles.cardLikeButton_liked}`
+                : styles.cardLikeButton
+            }
+            onClick={handleLikeClick}
+          />
+          <h2 className={styles.cardTitle}>{id}</h2>
+        </div>
+        {isHovered ? (
+          <button
+            className={styles.trash_icon}
+            onClick={() => dispatch(removeCard({ id }))}
+          />
+        ) : null}
+      </article>
+    </Link>
   );
 }
 
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,24 +1,22 @@
 import { useEffect } from 'react';
 
-import { useGetCardsQuery } from '../../services/CardsService';
-import { ICard } from '../../types/ICard';
+import { fetchCards } from '../../store/cardsSlice';
+import { useAppDispatch, useAppSelector } from '../../store/store';
+import { ICard } from '../../types/types';
 import Card from '../Card/Card';
 import styles from './styles/styles.module.css';
 
 function CardList() {
-  const { data, error, isLoading } = useGetCardsQuery(20);
+  const dispatch = useAppDispatch();
+  const cards = useAppSelector((store) => store.cards.data);
 
   useEffect(() => {
-    if (data) {
-      console.log(data);
-    }
-  }, [data]);
-
-  if (isLoading) return <div>Loading...</div>;
+    dispatch(fetchCards());
+  }, [dispatch]);
 
   return (
     <div className={styles.cardsContainer}>
-      {data?.map((card: ICard) => <Card id={card.id} image={card.url} key={card.id} />)}
+      {cards?.map((card: ICard) => <Card key={card.id} {...card} />)}
     </div>
   );
 }
